7afbfd7080fb4dcbd8b89b6f2a097c54fd8d49b3
Callteq <71952464+Callteq@users.noreply.github.com>
2020-09-30 21:42:57 +1000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def on_button_pressed_a():
+def LedDisplay(Lane: number, Position: number):
+    global LedPos, range2
+    LedPos = (Lane - 1) * 4
+    range2 = strip.range(LedPos, Position)
+    if Position == 1:
+        range2.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    elif Position == 2:
+        range2.show_color(neopixel.colors(NeoPixelColors.RED))
+    elif Position == 3:
+        range2.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    else:
+        range2.show_color(neopixel.colors(NeoPixelColors.WHITE))
+    range2.show()
+
+def on_button_pressed_ab():
     global Place, Lane1, Lane2, Lane3, Lane4
     Place = 0
     Lane1 = 0
@@ -8,37 +22,32 @@ def on_button_pressed_a():
     basic.show_icon(IconNames.SQUARE)
     basic.show_icon(IconNames.SMALL_SQUARE)
     basic.show_icon(IconNames.SMALL_DIAMOND)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 Lane4 = 0
 Lane3 = 0
 Lane2 = 0
 Lane1 = 0
 Place = 0
+range2: neopixel.Strip = None
+LedPos = 0
+strip: neopixel.Strip = None
 basic.show_icon(IconNames.NO)
-pins.set_pull(DigitalPin.P0, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P1, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P8, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P2, PinPullMode.PULL_NONE)
+pins.set_pull(DigitalPin.P16, PinPullMode.PULL_NONE)
+strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+L1 = strip.range(0, 3)
+L2 = strip.range(4, 7)
+L3 = strip.range(8, 11)
+L4 = strip.range(12, 15)
+strip.show_color(neopixel.colors(NeoPixelColors.WHITE))
+strip.show()
 
 def on_forever():
-    global Place, Lane1
-    if pins.digital_read_pin(DigitalPin.P1) == 0:
-        if Lane1 == 0:
-            basic.show_leds("""
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .
-                """)
-            Place += 1
-            Lane1 = Place
-basic.forever(on_forever)
-
-def on_forever2():
     global Place, Lane2
-    if pins.digital_read_pin(DigitalPin.P2) == 0:
+    if input.button_is_pressed(Button.B):
         if Lane2 == 0:
             basic.show_leds("""
                 . . . . .
@@ -49,9 +58,10 @@ def on_forever2():
                 """)
             Place += 1
             Lane2 = Place
-basic.forever(on_forever2)
+            LedDisplay(2, Lane2)
+basic.forever(on_forever)
 
-def on_forever3():
+def on_forever2():
     global Place
     if Place == 2:
         images.create_big_image("""
@@ -72,4 +82,20 @@ def on_forever3():
         led.plot(0, Lane1)
         led.plot(1, Lane2)
         Place += 1
+basic.forever(on_forever2)
+
+def on_forever3():
+    global Place, Lane1
+    if input.button_is_pressed(Button.A):
+        if Lane1 == 0:
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                """)
+            Place += 1
+            Lane1 = Place
+            LedDisplay(1, Lane1)
 basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
-input.onButtonPressed(Button.A, function () {
+function LedDisplay (Lane: number, Position: number) {
+    LedPos = (Lane - 1) * 4
+    range2 = strip.range(LedPos, Position)
+    if (Position == 1) {
+        range2.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (Position == 2) {
+        range2.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (Position == 3) {
+        range2.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else {
+        range2.showColor(neopixel.colors(NeoPixelColors.White))
+    }
+    range2.show()
+}
+input.onButtonPressed(Button.AB, function () {
     Place = 0
     Lane1 = 0
     Lane2 = 0
@@ -7,34 +21,26 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Square)
     basic.showIcon(IconNames.SmallSquare)
     basic.showIcon(IconNames.SmallDiamond)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
 let Lane4 = 0
 let Lane3 = 0
 let Lane2 = 0
 let Lane1 = 0
 let Place = 0
+let range2: neopixel.Strip = null
+let LedPos = 0
+let strip: neopixel.Strip = null
 basic.showIcon(IconNames.No)
-pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+strip.show()
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) == 0) {
-        if (Lane1 == 0) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .
-                `)
-            Place += 1
-            Lane1 = Place
-        }
-    }
-})
-basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
+    if (input.buttonIsPressed(Button.B)) {
         if (Lane2 == 0) {
             basic.showLeds(`
                 . . . . .
@@ -45,11 +51,18 @@ basic.forever(function () {
                 `)
             Place += 1
             Lane2 = Place
+            LedDisplay(2, Lane2)
+            Place += 1
+            Lane3 = Place
+            LedDisplay(3, Lane3)
+            Place += 1
+            Lane4 = Place
+            LedDisplay(4, Lane4)
         }
     }
 })
 basic.forever(function () {
-    if (Place == 2) {
+    if (Place == 4) {
         images.createBigImage(`
             # . # . # . # . # .
             . # . # . # . # . #
@@ -67,6 +80,24 @@ basic.forever(function () {
             `)
         led.plot(0, Lane1)
         led.plot(1, Lane2)
+        led.plot(3, Lane3)
+        led.plot(4, Lane4)
         Place += 1
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        if (Lane1 == 0) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                `)
+            Place += 1
+            Lane1 = Place
+            LedDisplay(1, Lane1)
+        }
     }
 })
