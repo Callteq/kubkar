7524bf925d7a44bbbe8c20a404b76b4bca82e876
Callteq <71952464+Callteq@users.noreply.github.com>
2020-10-06 21:27:56 +1100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
-function LedDisplay (Lane: number, Position: number, Mode: number) {
-    // indicate which lane
-    led.plot(Lane, 1)
+# udpate LEDs
+# Mode =1 is to display a lane has finsihe dthe race but not show the finish position
+# Mode =2 displays the finish positon for each Lane
+def LedDisplay(Lane: number, Position: number, Mode: number):
+    global firstKar, LedPos, winnerStrip, haveWinner, secondStrip, thirdStrip, fourthStrip
+    if firstKar == 0:
+        firstKar = 1
+        basic.clear_screen()
     LedPos = (Lane - 1) * 4
-    if (Mode == 1) {
+    if Mode == 1:
         laneStrip = strip.range(LedPos, 1)
-        laneStrip.showColor(neopixel.colors(NeoPixelColors.Purple))
-    } else {
-        laneStrip = strip.range(LedPos, 4)
-        if (Position == 1) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        laneStrip.show_color(neopixel.colors(NeoPixelColors.PURPLE))
+        # indicate which lane we have seen finsih the race
+        led.plot(Lane, 1)
+    else:
+        # indicate final positions
+        led.plot(Lane, Position)
+        if Position == 1:
             winnerStrip = strip.range(LedPos, 4)
-            winnerStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
+            winnerStrip.show_color(neopixel.colors(NeoPixelColors.BLUE))
             haveWinner = 1
-        } else if (Position == 2) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Red))
-        } else if (Position == 3) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Green))
-        } else {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.White))
-        }
-    }
-    laneStrip.show()
-}
-// Reset the race
-input.onButtonPressed(Button.A, function () {
-    // initiialise
+        elif Position == 2:
+            secondStrip = strip.range(LedPos, 2)
+            secondStrip.show_color(neopixel.colors(NeoPixelColors.RED))
+        elif Position == 3:
+            thirdStrip = strip.range(LedPos, 3)
+            thirdStrip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        else:
+            fourthStrip = strip.range(LedPos, 4)
+            fourthStrip.show_color(neopixel.colors(NeoPixelColors.ORANGE))
+# Reset the race
+
+def on_button_pressed_a():
+    global thePlace, Lane1Place, Lane2Place, Lane3Place, Lane4Place, Diag, haveWinner, firstKar
     thePlace = 99
     Lane1Place = 0
     Lane2Place = 0
@@ -32,82 +39,174 @@ input.onButtonPressed(Button.A, function () {
     Lane4Place = 0
     Diag = 0
     haveWinner = 0
-    basic.showIcon(IconNames.Square)
-    basic.showIcon(IconNames.SmallSquare)
+    firstKar = 0
+    # basic.show_icon(IconNames.SQUARE)
+    basic.show_icon(IconNames.SMALL_SQUARE)
+    #basic.show_icon(IconNames.SMALL_DIAMOND)
+    for index in range(3):
+        strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+        strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.GREEN))
+        strip.set_pixel_color(4, neopixel.colors(NeoPixelColors.GREEN))
+        strip.set_pixel_color(8, neopixel.colors(NeoPixelColors.GREEN))
+        strip.set_pixel_color(12, neopixel.colors(NeoPixelColors.GREEN))
+        index2 = 0
+        while index2 < 4:
+            basic.pause(30)
+            strip.shift(1)
+            strip.show()
+            index2 += 1
+        index2 = 0
+        while index2 < 4:
+            basic.pause(100)
+            strip.shift(-1)
+            strip.show()
+            index2 += 1
+    index2 = 0
+    while index2 < 8:
+        basic.pause(50)
+        strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        strip.show()
+        basic.pause(100)
+        strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+        strip.show()
+        index2 += 1
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    basic.clear_screen()
+    basic.show_icon(IconNames.HAPPY)
     thePlace = 0
-    basic.showIcon(IconNames.SmallDiamond)
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-})
-// simulate the cars crossing the finsih line one ach press of Button B
-input.onButtonPressed(Button.B, function () {
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# simulate the cars crossing the finish line one Kar on each press of Button B
+
+def on_button_pressed_b():
+    global Diag, thePlace, Lane1Place, Lane2Place, Lane3Place, Lane4Place
     Diag = Diag + 1
-    if (Diag == 1) {
+    if  Lane1Place == 0:
         thePlace = thePlace + 1
         Lane1Place = thePlace
         LedDisplay(1, Lane1Place, 1)
-    } else if (Diag == 2) {
+    elif Lane2Place == 0:
         thePlace = thePlace + 1
         Lane2Place = thePlace
         LedDisplay(2, Lane2Place, 1)
-    } else if (Diag == 3) {
+    elif Lane3Place == 0:
         thePlace = thePlace + 1
         Lane3Place = thePlace
         LedDisplay(3, Lane3Place, 1)
-    } else if (Diag == 4) {
+    elif Lane4Place == 0:
         thePlace = thePlace + 1
         Lane4Place = thePlace
         LedDisplay(4, Lane4Place, 1)
-    }
-})
-let Diag = 0
-let Lane4Place = 0
-let Lane3Place = 0
-let Lane2Place = 0
-let Lane1Place = 0
-let thePlace = 0
-let winnerStrip: neopixel.Strip = null
-let laneStrip: neopixel.Strip = null
-let LedPos = 0
-let strip: neopixel.Strip = null
-let haveWinner = 0
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+Diag = 0
+Lane4Place = 0
+Lane3Place = 0
+Lane2Place = 0
+Lane1Place = 0
+thePlace = 0
+LedPos = 0
+fourthStrip: neopixel.Strip = None
+thirdStrip: neopixel.Strip = None
+secondStrip: neopixel.Strip = None
+winnerStrip: neopixel.Strip = None
+strip: neopixel.Strip = None
 haveWinner = 0
-basic.showIcon(IconNames.No)
-pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+firstKar = 0
+firstKar = 0
+haveWinner = 0
+firstKar = 0
+basic.show_icon(IconNames.NO)
+pins.set_pull(DigitalPin.P1, PinPullMode.PULL_DOWN)
+# Lane 1 Sensor
+pins.set_pull(DigitalPin.P8, PinPullMode.PULL_DOWN)
+# Lane 2 Sensor
+pins.set_pull(DigitalPin.P15, PinPullMode.PULL_DOWN)
+# Lane 3 Sensor
+pins.set_pull(DigitalPin.P16, PinPullMode.PULL_DOWN)
+# Lane 4 Sensor
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Black))
+winnerStrip = strip.range(0, 4)
+secondStrip = strip.range(4, 4)
+thirdStrip = strip.range(8, 4)
+fourthStrip = strip.range(12, 4)
+strip.show_rainbow(1, 360)
+# winnerStrip.show_color(neopixel.colors(NeoPixelColors.ORANGE))
 strip.show()
-basic.forever(function () {
-    if (haveWinner == 1) {
-        basic.pause(50)
-        winnerStrip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(50)
-    }
-})
-// wait until we have all 4 cars across the finish line
-basic.forever(function () {
-    if (thePlace == 4) {
-        images.createBigImage(`
+
+def on_forever():
+    global thePlace
+    if haveWinner == 1:
+        i = 0
+        while i < 10:
+            basic.pause(100)
+            winnerStrip.show_color(neopixel.colors(NeoPixelColors.WHITE))
+            basic.pause(100)
+            winnerStrip.show_color(neopixel.colors(NeoPixelColors.BLUE))
+            winnerStrip.show()
+            i += 1
+        winnerStrip.set_pixel_color(0, neopixel.colors(NeoPixelColors.BLACK))
+        winnerStrip.set_pixel_color(1, neopixel.colors(NeoPixelColors.BLACK))
+        winnerStrip.set_pixel_color(2, neopixel.colors(NeoPixelColors.BLACK))
+        winnerStrip.set_pixel_color(3, neopixel.colors(NeoPixelColors.BLUE))
+        winnerStrip.show()
+        i = 0
+        while i < 24:
+            basic.pause(100)
+            winnerStrip.rotate(1)
+            winnerStrip.show()
+            i += 1
+    if thePlace == 4:
+        basic.pause(1000)
+        images.create_big_image("""
             # . # . # . # . # .
             . # . # . # . # . #
             # . # . # . # . # .
             . # . # . # . # . #
             # . # . # . # . # .
-            `).scrollImage(1, 50)
-        control.waitMicros(100)
-        basic.showLeds(`
-            # # # # #
+            """).scroll_image(1, 50)
+        basic.show_leds("""
+            . # # # #
             . . . . .
             . . . . .
             . . . . .
             . . . . .
-            `)
+            """)
+        # display final positions
         LedDisplay(1, Lane1Place, 2)
         LedDisplay(2, Lane2Place, 2)
         LedDisplay(3, Lane3Place, 2)
         LedDisplay(4, Lane4Place, 2)
         thePlace = thePlace + 1
-    }
-})
+basic.forever(on_forever)
+
+# check the track sensors
+
+def on_forever2():
+    global thePlace, Lane1Place, Lane2Place, Lane3Place, Lane4Place
+    # Lane 1
+    if pins.digital_read_pin(DigitalPin.P1) == 1:
+        if Lane1Place == 0:
+            thePlace = thePlace + 1
+            Lane1Place = thePlace
+            LedDisplay(1, Lane1Place, 1)
+    # Lane 2
+    if pins.digital_read_pin(DigitalPin.P8) == 1:
+        basic.pause(1000)
+        if Lane2Place == 0:
+            thePlace = thePlace + 1
+            Lane2Place = thePlace
+            LedDisplay(2, Lane2Place, 1)
+    # Lane 3
+    if pins.digital_read_pin(DigitalPin.P15) == 1:
+        if Lane3Place == 0:
+            thePlace = thePlace + 1
+            Lane3Place = thePlace
+            LedDisplay(3, Lane3Place, 1)
+    # Lane 4
+    if pins.digital_read_pin(DigitalPin.P16) == 1:
+        if Lane4Place == 0:
+            thePlace = thePlace + 1
+            Lane4Place = thePlace
+            LedDisplay(4, Lane4Place, 1)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,59 +1,103 @@
+// udpate LEDs
+// Mode =1 is to display a lane has finsihe dthe race but not show the finish position
+// Mode =2 displays the finish positon for each Lane
 function LedDisplay (Lane: number, Position: number, Mode: number) {
-    // indicate which lane
-    led.plot(Lane, 1)
+    let laneStrip: neopixel.Strip;
+if (firstKar == 0) {
+        firstKar = 1
+        basic.clearScreen()
+    }
     LedPos = (Lane - 1) * 4
     if (Mode == 1) {
         laneStrip = strip.range(LedPos, 1)
         laneStrip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        // indicate which lane we have seen finsih the race
+        led.plot(Lane, 1)
     } else {
-        laneStrip = strip.range(LedPos, 4)
+        // indicate final positions
+        led.plot(Lane, Position)
         if (Position == 1) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
             winnerStrip = strip.range(LedPos, 4)
             winnerStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
             haveWinner = 1
         } else if (Position == 2) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Red))
+            secondStrip = strip.range(LedPos, 2)
+            secondStrip.showColor(neopixel.colors(NeoPixelColors.Red))
         } else if (Position == 3) {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.Green))
+            thirdStrip = strip.range(LedPos, 3)
+            thirdStrip.showColor(neopixel.colors(NeoPixelColors.Green))
         } else {
-            laneStrip.showColor(neopixel.colors(NeoPixelColors.White))
+            fourthStrip = strip.range(LedPos, 4)
+            fourthStrip.showColor(neopixel.colors(NeoPixelColors.Orange))
         }
     }
-    laneStrip.show()
 }
 // Reset the race
 input.onButtonPressed(Button.A, function () {
-    // initiialise
-    thePlace = 99
+    let index2: number;
+thePlace = 99
     Lane1Place = 0
     Lane2Place = 0
     Lane3Place = 0
     Lane4Place = 0
     Diag = 0
     haveWinner = 0
-    basic.showIcon(IconNames.Square)
+    firstKar = 0
+    // basic.show_icon(IconNames.SQUARE)
     basic.showIcon(IconNames.SmallSquare)
-    thePlace = 0
-    basic.showIcon(IconNames.SmallDiamond)
+    // basic.show_icon(IconNames.SMALL_DIAMOND)
+    for (let index = 0; index < 3; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Green))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Green))
+        index2 = 0
+        while (index2 < 4) {
+            basic.pause(30)
+            strip.shift(1)
+            strip.show()
+            index2 += 1
+        }
+        index2 = 0
+        while (index2 < 4) {
+            basic.pause(100)
+            strip.shift(-1)
+            strip.show()
+            index2 += 1
+        }
+    }
+    index2 = 0
+    while (index2 < 8) {
+        basic.pause(50)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+        basic.pause(100)
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip.show()
+        index2 += 1
+    }
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+    thePlace = 0
 })
-// simulate the cars crossing the finsih line one ach press of Button B
+// simulate the cars crossing the finish line one Kar on each press of Button B
 input.onButtonPressed(Button.B, function () {
     Diag = Diag + 1
-    if (Diag == 1) {
+    if (Lane1Place == 0) {
         thePlace = thePlace + 1
         Lane1Place = thePlace
         LedDisplay(1, Lane1Place, 1)
-    } else if (Diag == 2) {
+    } else if (Lane2Place == 0) {
         thePlace = thePlace + 1
         Lane2Place = thePlace
         LedDisplay(2, Lane2Place, 1)
-    } else if (Diag == 3) {
+    } else if (Lane3Place == 0) {
         thePlace = thePlace + 1
         Lane3Place = thePlace
         LedDisplay(3, Lane3Place, 1)
-    } else if (Diag == 4) {
+    } else if (Lane4Place == 0) {
         thePlace = thePlace + 1
         Lane4Place = thePlace
         LedDisplay(4, Lane4Place, 1)
@@ -65,31 +109,61 @@ let Lane3Place = 0
 let Lane2Place = 0
 let Lane1Place = 0
 let thePlace = 0
-let winnerStrip: neopixel.Strip = null
-let laneStrip: neopixel.Strip = null
 let LedPos = 0
+let fourthStrip: neopixel.Strip = null
+let thirdStrip: neopixel.Strip = null
+let secondStrip: neopixel.Strip = null
+let winnerStrip: neopixel.Strip = null
 let strip: neopixel.Strip = null
 let haveWinner = 0
+let firstKar = 0
+firstKar = 0
 haveWinner = 0
+firstKar = 0
 basic.showIcon(IconNames.No)
-pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
+// Lane 1 Sensor
+pins.setPull(DigitalPin.P8, PinPullMode.PullDown)
+// Lane 2 Sensor
+pins.setPull(DigitalPin.P15, PinPullMode.PullDown)
+// Lane 3 Sensor
+pins.setPull(DigitalPin.P16, PinPullMode.PullDown)
+// Lane 4 Sensor
 strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Black))
+winnerStrip = strip.range(0, 4)
+secondStrip = strip.range(4, 4)
+thirdStrip = strip.range(8, 4)
+fourthStrip = strip.range(12, 4)
+strip.showRainbow(1, 360)
+// winnerStrip.show_color(neopixel.colors(NeoPixelColors.ORANGE))
 strip.show()
 basic.forever(function () {
-    if (haveWinner == 1) {
-        basic.pause(200)
-        winnerStrip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-        basic.pause(200)
-        winnerStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    let i: number;
+if (haveWinner == 1) {
+        i = 0
+        while (i < 10) {
+            basic.pause(100)
+            winnerStrip.showColor(neopixel.colors(NeoPixelColors.White))
+            basic.pause(100)
+            winnerStrip.showColor(neopixel.colors(NeoPixelColors.Blue))
+            winnerStrip.show()
+            i += 1
+        }
+        winnerStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        winnerStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        winnerStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        winnerStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        winnerStrip.show()
+        i = 0
+        while (i < 24) {
+            basic.pause(100)
+            winnerStrip.rotate(1)
+            winnerStrip.show()
+            i += 1
+        }
     }
-})
-// wait until we have all 4 cars across the finish line
-basic.forever(function () {
     if (thePlace == 4) {
+        basic.pause(1000)
         images.createBigImage(`
             # . # . # . # . # .
             . # . # . # . # . #
@@ -97,18 +171,54 @@ basic.forever(function () {
             . # . # . # . # . #
             # . # . # . # . # .
             `).scrollImage(1, 50)
-        control.waitMicros(100)
         basic.showLeds(`
-            # # # # #
+            . # # # #
             . . . . .
             . . . . .
             . . . . .
             . . . . .
             `)
+        // display final positions
         LedDisplay(1, Lane1Place, 2)
         LedDisplay(2, Lane2Place, 2)
         LedDisplay(3, Lane3Place, 2)
         LedDisplay(4, Lane4Place, 2)
         thePlace = thePlace + 1
+    }
+})
+// check the track sensors
+basic.forever(function () {
+    // Lane 1
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        if (Lane1Place == 0) {
+            thePlace = thePlace + 1
+            Lane1Place = thePlace
+            LedDisplay(1, Lane1Place, 1)
+        }
+    }
+    // Lane 2
+    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        basic.pause(1000)
+        if (Lane2Place == 0) {
+            thePlace = thePlace + 1
+            Lane2Place = thePlace
+            LedDisplay(2, Lane2Place, 1)
+        }
+    }
+    // Lane 3
+    if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        if (Lane3Place == 0) {
+            thePlace = thePlace + 1
+            Lane3Place = thePlace
+            LedDisplay(3, Lane3Place, 1)
+        }
+    }
+    // Lane 4
+    if (pins.digitalReadPin(DigitalPin.P16) == 1) {
+        if (Lane4Place == 0) {
+            thePlace = thePlace + 1
+            Lane4Place = thePlace
+            LedDisplay(4, Lane4Place, 1)
+        }
     }
 })
